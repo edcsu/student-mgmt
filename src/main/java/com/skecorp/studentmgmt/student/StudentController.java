c79183ba2bb79c450b4cb2d477102172a193570c
Sewannonda Keith Edwin <skeith696@gmail.com>
2022-07-29 20:35:45 +0300

    feat: add student controller

---
+++ src/main/java/com/skecorp/studentmgmt/student/StudentController.java
@@ -0,0 +1,22 @@
+package com.skecorp.studentmgmt.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/students")
+public class StudentController {
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        List<Student> studentList = Arrays.asList(
+                new Student(1L, "John Doe", "johndoe@skeuni.co.ug", Gender.MALE),
+                new Student(1L, "Jane Doe", "janedoe@skeuni.co.ug", Gender.FEMALE)
+        );
+        return studentList;
+    }
+}
