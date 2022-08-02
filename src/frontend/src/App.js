78a7df2b58d25740513e0fc4aff0c5027e4dd189
Sewannonda Keith Edwin <skeith696@gmail.com>
2022-08-02 22:31:44 +0300

    style: clean front-end

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -208,13 +208,6 @@ function App() {
                     <Menu.Item key="4">Bill</Menu.Item>
                     <Menu.Item key="5">Alex</Menu.Item>
                 </Menu.SubMenu>
-                <Menu.SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
-                    <Menu.Item key="6">Team 1</Menu.Item>
-                    <Menu.Item key="8">Team 2</Menu.Item>
-                </Menu.SubMenu>
-                <Menu.Item key="9" icon={<FileOutlined />}>
-                    Files
-                </Menu.Item>
             </Menu>
         </Sider>
         <Layout className="site-layout">
